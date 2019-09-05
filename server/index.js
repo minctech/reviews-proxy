fce54491b91a24fb3d17684f127a6ae5ca415c50
Anthony Fojas <anthonyjfojas@gmail.com>
2019-09-04 19:21:52 -0700

    Proxy reviews, reservations, and recommendations

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+app.use('/listings/:listing_id', express.static('./public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
