c4286d5982c32e1640a22e2b11a8ecc277fe348c
Anthony Fojas <anthonyjfojas@gmail.com>
2019-09-08 14:19:45 -0700

    Improve load performance

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const compression = require('compression');
 
+app.use(compression());
 app.use('/listings/:listing_id', express.static('./public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
